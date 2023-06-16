cbd7744cd93a0ea0d30b19bbb38e05545dbae78e
Ayush Jain <ayushjain2205@gmail.com>
2023-06-16 15:52:06 +0530

    Create page

--- components/form/FileButton.tsx
+++ components/form/FileButton.tsx
@@ -2,30 +2,36 @@ import React from 'react';
 import type { UploadProps } from 'antd';
 import { message, Upload } from 'antd';
 
-const props: UploadProps = {
-  name: 'file',
-  action: 'https://api.escuelajs.co/api/v1/files/upload',
-  headers: {
-    authorization: 'authorization-text',
-  },
-  onChange(info) {
-    if (info.file.status !== 'uploading') {
-      console.log(info.file, info.fileList);
-    }
-    if (info.file.status === 'done') {
-      message.success(`${info.file.name} file uploaded successfully`);
-    } else if (info.file.status === 'error') {
-      message.error(`${info.file.name} file upload failed.`);
-    }
-  },
-};
+interface FileButtonProps {
+  buttonText: string;
+}
 
-const FileButton: React.FC = () => (
-  <Upload {...props}>
-    <button className="h-[64px] w-[230px] text-white bg-black text-[24px]">
-      add Resources
-    </button>
-  </Upload>
-);
+const FileButton: React.FC<FileButtonProps> = ({ buttonText }) => {
+  const props: UploadProps = {
+    name: 'file',
+    action: 'https://api.escuelajs.co/api/v1/files/upload',
+    headers: {
+      authorization: 'authorization-text',
+    },
+    onChange(info) {
+      if (info.file.status !== 'uploading') {
+        console.log(info.file, info.fileList);
+      }
+      if (info.file.status === 'done') {
+        message.success(`${info.file.name} file uploaded successfully`);
+      } else if (info.file.status === 'error') {
+        message.error(`${info.file.name} file upload failed.`);
+      }
+    },
+  };
+
+  return (
+    <Upload {...props}>
+      <button className="h-[64px] w-[230px] text-white bg-black text-[24px]">
+        {buttonText}
+      </button>
+    </Upload>
+  );
+};
 
 export default FileButton;
--- pages/create.tsx
+++ pages/create.tsx
@@ -1,8 +1,43 @@
 import React from 'react';
 import Layout from '@/components/layout';
+import BoxHeader from '@/components/ui/BoxHeader';
+import FileButton from '@/components/form/FileButton';
 
 const Create = () => {
-  return <Layout>create</Layout>;
+  return (
+    <Layout>
+      <div className="flex flex-col">
+        <h5 className="font-bold text-[36px] my-[20px]">LeT’S create</h5>
+        <div className="flex flex-row gap-[24px]">
+          <div className="flex flex-col gap-[24px]">
+            <div className="h-[123px] w-[346px] border-2 border-black rounded-[4px] flex flex-col justify-center items-center  ">
+              <input
+                type="text"
+                placeholder="enter name here"
+                className="input input-ghost w-[237px] max-w-xs focus:border-transparent focus:outline-none"
+              />
+            </div>
+            <div className="h-[323px] w-[346px] border-2 border-black rounded-[4px] flex flex-col justify-center items-center gap-[24px] ">
+              <p className="text-[24px] w-[237px]">
+                what does your BOT look like?
+              </p>
+              <FileButton buttonText="add" />
+            </div>
+          </div>
+          <div className="w-[782px]">
+            <BoxHeader title="Knowledge base" bg="#fff">
+              <div className="h-[350px] flex flex-col justify-center items-center">
+                <FileButton buttonText="add Resources" />
+                <span className="italic text-black text-opacity-50 text-[24px]">
+                  (pdf, csv, video link, ppt, xls & more....)
+                </span>
+              </div>
+            </BoxHeader>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
 };
 
 export default Create;
