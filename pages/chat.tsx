53ad7b2c1e54fd04f117075ee0f7fde5180a860e
Ayush Jain <ayushjain2205@gmail.com>
2023-06-17 02:58:42 +0530

    Add chat screen top UI title and dropdown menu

--- pages/chat.tsx
+++ pages/chat.tsx
@@ -17,6 +17,28 @@ const Chatpage = () => {
 
   return (
     <Layout>
+      <div className="flex flex-row w-[1152px] justify-between mb-[36px] mt-[25px]">
+        <h5 className="font-bold text-[36px]">The PHARMA man</h5>
+        <div className="dropdown dropdown-end">
+          <label tabIndex={0} className="cursor-pointer">
+            <img src="/more-vertical.svg" alt="" />
+          </label>
+          <ul
+            tabIndex={0}
+            className="dropdown-content menu p-2 shadow bg-black text-white text-[16px] rounded-[2px] w-52"
+          >
+            <li>
+              <a>uSE api</a>
+            </li>
+            <li>
+              <a>download extension</a>
+            </li>
+            <li>
+              <a>didget for mobil</a>
+            </li>
+          </ul>
+        </div>
+      </div>
       <ChatBot />
       <dialog id="buy_modal" className="modal">
         <form
