2bb242ab7f9c820d44fc1ba28bd4fb3cf2241cdd
Ayush Jain <ayushjain2205@gmail.com>
2023-06-18 03:24:09 +0530

    Add buy coins popup to navbar

--- web/components/ui/Navbar.tsx
+++ web/components/ui/Navbar.tsx
@@ -3,12 +3,51 @@ import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import React, { useEffect, useState } from 'react';
 import { ConnectWallet, useAddress } from '@thirdweb-dev/react';
+import * as Popover from '@radix-ui/react-popover';
+import { InputNumber } from 'antd';
+import { ethers } from 'ethers';
+import { abi } from '../../config/abi';
 
 const Navbar = () => {
-  const [active, setActive] = useState('');
-  const pathname = usePathname();
+  const [contract, setContract] = useState<ethers.Contract | null>(null);
+  const [inputValue, setInputValue] = useState(100);
 
   const address = useAddress();
+
+  const contractAddress = '0x0603dCfcAF81Df618915EcA57F4fa2c2ee50FDE3';
+
+  const contractABI = abi;
+
+  useEffect(() => {
+    const provider = new ethers.providers.Web3Provider(
+      window.ethereum as ethers.providers.ExternalProvider,
+    );
+
+    const signer = provider.getSigner();
+
+    const contractInstance = new ethers.Contract(
+      contractAddress,
+      contractABI,
+      signer,
+    );
+
+    setContract(contractInstance);
+  }, []);
+
+  async function mintTokens() {
+    if (!contract) {
+      console.error('Contract is not loaded');
+      return;
+    }
+
+    let to = address || '0xCafa93E9985793E2475bD58B9215c21Dbd421fD0';
+    const amount = ethers.utils.parseUnits(inputValue.toString(), 18); // The amount of b-coins to be minted
+
+    await contract.mint(to, amount);
+  }
+
+  const [active, setActive] = useState('');
+  const pathname = usePathname();
 
   useEffect(() => {
     setActive(pathname);
@@ -62,7 +101,51 @@ const Navbar = () => {
             alt=""
           />
         )}
-        <ConnectWallet theme="light" />
+        <div className="flex flex-row justify-center items-center gap-[10px]">
+          <Popover.Root>
+            <Popover.Trigger asChild>
+              <button className="flex flex-row justify-center items-center h-[48px] w-[151px] text-white bg-black text-[16px]">
+                Buy coins
+              </button>
+            </Popover.Trigger>
+            <Popover.Portal>
+              <Popover.Content
+                className="rounded p-5 w-[311px] h-[252px] bg-black shadow-[0_10px_38px_-10px_hsla(206,22%,7%,.35),0_10px_20px_-15px_hsla(206,22%,7%,.2)] focus:shadow-[0_10px_38px_-10px_hsla(206,22%,7%,.35),0_10px_20px_-15px_hsla(206,22%,7%,.2),0_0_0_2px_theme(colors.violet7)] will-change-[transform,opacity] data-[state=open]:data-[side=top]:animate-slideDownAndFade data-[state=open]:data-[side=right]:animate-slideLeftAndFade data-[state=open]:data-[side=bottom]:animate-slideUpAndFade data-[state=open]:data-[side=left]:animate-slideRightAndFade"
+                sideOffset={5}
+              >
+                <p className="font-bold text-[24px] text-[#F24E1E]">
+                  10 B-coins = $1
+                </p>
+                <p className="text-[18px] text-white mt-[16px]">
+                  let’s purchase?
+                </p>
+                <InputNumber
+                  min={0}
+                  max={1000}
+                  defaultValue={100}
+                  className="mt-[8px] input input-bordered w-[279px] h-[48px] flex items-center hover:border-[#262626] hover:border-opacity-40 overflow-hidden rounded-[2px]"
+                  onChange={(value) => {
+                    if (value !== null) {
+                      setInputValue(value);
+                    }
+                  }}
+                />
+                <div className="flex flex-row justify-between w-[279px] mt-[24px]">
+                  <img src="/stars.svg" alt="" />
+                  <button
+                    onClick={mintTokens}
+                    className="bg-[#F24E1E] h-[48px] w-[96px] font-bold text-white text-[16px] rounded-[2px]"
+                  >
+                    Buy now
+                  </button>
+                </div>
+
+                <Popover.Arrow className="fill-black" />
+              </Popover.Content>
+            </Popover.Portal>
+          </Popover.Root>
+          <ConnectWallet theme="light" />
+        </div>
       </div>
     </div>
   );
