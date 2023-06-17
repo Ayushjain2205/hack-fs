234a5bb082203b7ec63b71d7a202107daefb6528
Ayush Jain <ayushjain2205@gmail.com>
2023-06-18 04:50:32 +0530

    Update dashboard page

--- web/pages/dashboard.tsx
+++ web/pages/dashboard.tsx
@@ -7,6 +7,12 @@ const Dashboard = () => {
   return (
     <Layout>
       <p className="font-bold text-[36px]">your bot analytics</p>
+      <div className="flex flex-row">
+        <div className="mb-[32px] mt-[24px] flex flex-row gap-[20px] leading-[30px] p-[23px] h-[138px] w-[240px] border-black border-2 rounded-[4px] bg-[#FFC022]">
+          <div className="w-[90px] text-[24px]">all HEALTH MAN</div>
+          <img src="/mascot.svg" alt="" />
+        </div>
+      </div>
       <TabsHolder />
     </Layout>
   );
