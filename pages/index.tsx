3db39d100379c7162b97391cc6c2011b52addfa1
Ayush Jain <ayushjain2205@gmail.com>
2023-06-17 12:37:18 +0530

    Add one landing circle

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,13 @@ import React from 'react';
 import Layout from '@/components/layout';
 
 const Landing = () => {
-  return <Layout>Landing</Layout>;
+  return (
+    <Layout>
+      <div>
+        <div className="h-[300px] w-[300px] bg-[#FFC022] rounded-full border-black border-[2px]"></div>
+      </div>
+    </Layout>
+  );
 };
 
 export default Landing;
