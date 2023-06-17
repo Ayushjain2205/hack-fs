38275702d87a6760596212f623defcea37a2f0b5
Ayush Jain <ayushjain2205@gmail.com>
2023-06-17 16:27:07 +0530

    Update breaking effect of landing page on other pages

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,12 +1,14 @@
 import Navbar from '@/components/ui/Navbar';
 import React from 'react';
 
+import styles from '../styles/landing.module.css';
+
 const HexagonalGrid = () => {
   return (
     <div className="mx-auto flex flex-col space-y-4">
       <Navbar />
-      <div className="main">
-        <div className="container">
+      <div className={styles.main}>
+        <div className={styles.container}>
           <div className="flex flex-row items-center justify-center">
             <img className="m-auto mt-[25px]" src="/landing1.svg" alt="" />
           </div>
